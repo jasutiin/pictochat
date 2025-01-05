8525cf47d523927aa4fe77cbe4dd2e9b6677153a
justine <justinemangaliman@gmail.com>
2025-01-05 16:43:21 -0700

    feat: make layout for room select, needs refactoring

--- frontend/src/pages/Room.vue
+++ frontend/src/pages/Room.vue
@@ -2,22 +2,89 @@
 
 <template>
   <div class="container">
-    <h1 class="item">Room {{ $route.params.id }}</h1>
-    <RouterLink to="/room-select" class="item">Back</RouterLink>
+    <div class="top-container">
+      <div class="white-bar">
+        <div class="square-top"></div>
+        <div class="square-bot"></div>
+      </div>
+      <div class="inner">
+        <div class="title">Pictochat</div>
+      </div>
+    </div>
+    <div class="bottom-container">
+      <h1 class="item">Room {{ $route.params.id }}</h1>
+      <RouterLink to="/room-select" class="item">Back</RouterLink>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .container {
-  width: 600px;
   display: flex;
+  width: 600px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-size: xx-large;
+  background-color: #c9c9c9;
+  background-size: 15px 15px;
+  background-image: linear-gradient(to bottom, white 1px, transparent 3px);
+}
+
+.bottom-container {
+  width: inherit;
+  display: flex;
+  flex-direction: column;
+  height: 50%;
+  justify-content: center;
+  align-items: center;
+}
+
+.top-container {
+  display: flex;
+  width: inherit;
+  height: 50%;
+}
+
+.title {
+  display: flex;
+  padding: 10px;
+  align-items: center;
+  height: 20px;
+  border: 3px solid white;
+  border-radius: 20px;
+  background-color: grey;
+  color: white;
+  margin: 10px;
+}
+
+.white-bar {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
   background-color: white;
-  background-size: 60px 60px;
-  background-image: linear-gradient(to right, #ababab 1px, transparent 3px),
-    linear-gradient(to bottom, #ababab 1px, transparent 3px);
+  width: 40px;
+}
+
+.inner {
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  height: 100%;
+  flex: 1;
+}
+
+.square-top {
+  width: 40px;
+  height: 40px;
+  border-bottom: 1px dotted black;
+}
+
+.square-bot {
+  width: 40px;
+  height: 40px;
+  border-top: 1px dotted black;
 }
 
 .item {
--- frontend/src/pages/RoomSelect.vue
+++ frontend/src/pages/RoomSelect.vue
@@ -2,17 +2,46 @@
 
 <template>
   <div class="container">
-    <div class="top-container"></div>
+    <div class="top-container">
+      <div class="white-bar">
+        <div class="square-top"></div>
+        <div class="square-bot"></div>
+      </div>
+      <div class="inner">
+        <div class="title">Pictochat</div>
+      </div>
+    </div>
     <div class="bottom-container">
       <div class="design">
-        Choose a Chat Room to join. <RouterLink to="/">Back</RouterLink>
+        <span class="head">Choose a Chat Room to join.</span>
+        <RouterLink to="/" class="back">Back</RouterLink>
       </div>
       <nav>
-        <RouterLink to="/room/1" class="item">Chat Room A</RouterLink>
-        <RouterLink to="/room/2" class="item">Chat Room B</RouterLink>
-        <RouterLink to="/room/3" class="item">Chat Room C</RouterLink>
-        <RouterLink to="/room/4" class="item">Chat Room D</RouterLink>
-        <RouterLink to="/room/5" class="item">Chat Room E</RouterLink>
+        <RouterLink to="/room/A" class="item">
+          <div class="letter">A</div>
+          Chat Room A
+          <div class="member-count">0/16</div>
+        </RouterLink>
+        <RouterLink to="/room/B" class="item">
+          <div class="letter">B</div>
+          Chat Room B
+          <div class="member-count">0/16</div>
+        </RouterLink>
+        <RouterLink to="/room/C" class="item">
+          <div class="letter">C</div>
+          Chat Room C
+          <div class="member-count">0/16</div>
+        </RouterLink>
+        <RouterLink to="/room/D" class="item">
+          <div class="letter">D</div>
+          Chat Room D
+          <div class="member-count">0/16</div>
+        </RouterLink>
+        <RouterLink to="/room/E" class="item">
+          <div class="letter">E</div>
+          Chat Room E
+          <div class="member-count">0/16</div>
+        </RouterLink>
       </nav>
       <div class="design"></div>
     </div>
@@ -23,11 +52,13 @@
 .container {
   display: flex;
   width: 600px;
-  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: xx-large;
+  background-color: #c9c9c9;
+  background-size: 15px 15px;
+  background-image: linear-gradient(to bottom, white 1px, transparent 3px);
 }
 
 .bottom-container {
@@ -37,24 +68,58 @@
   height: 50%;
   justify-content: center;
   align-items: center;
-  background-color: white;
-  background-size: 60px 60px;
-  background-image: linear-gradient(to right, #ababab 1px, transparent 3px),
-    linear-gradient(to bottom, #ababab 1px, transparent 3px);
 }
 
 .top-container {
+  display: flex;
   width: inherit;
   height: 50%;
+}
+
+.title {
+  display: flex;
+  padding: 10px;
+  align-items: center;
+  height: 20px;
+  border: 3px solid white;
+  border-radius: 20px;
+  background-color: grey;
+  color: white;
+  margin: 10px;
+}
+
+.white-bar {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
   background-color: white;
-  background-size: 60px 60px;
-  background-image: linear-gradient(to right, #ababab 1px, transparent 3px),
-    linear-gradient(to bottom, #ababab 1px, transparent 3px);
+  width: 40px;
+}
+
+.inner {
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  height: 100%;
+  flex: 1;
+}
+
+.square-top {
+  width: 40px;
+  height: 40px;
+  border-bottom: 1px dotted black;
+}
+
+.square-bot {
+  width: 40px;
+  height: 40px;
+  border-top: 1px dotted black;
 }
 
 .design {
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   height: 150px;
   background-color: royalblue;
@@ -62,8 +127,7 @@
 }
 
 nav {
-  display: flex;
-  /* position: absolute; */
+  padding: 30px 0px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -75,13 +139,44 @@ nav {
 .item {
   display: flex;
   align-items: center;
-  background-color: grey;
-  height: 300px;
+  background-color: #e2e2e2;
+  height: 85px;
+  width: 80%;
+  margin: auto;
+  border: 1px solid black;
 }
 
-@media (max-width: 426px) {
+.letter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: xxx-large;
+  width: 100px;
+  border: 1px solid black;
+  height: 100%;
+  margin-right: 20px;
+}
+
+.member-count {
+  padding: 5px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 100px;
+  border: 1px solid black;
+}
+
+@media (max-width: 600px) {
   .container {
     width: 100vw;
+  }
+}
+
+@media (max-width: 425px) {
+  .container {
+    width: 100vw;
+    font-size: x-large;
   }
 }
 </style>
